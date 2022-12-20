95315e79e084999380e07f20edaeff8168179b35
Roberto Sanchez <59806324+rsanc1103@users.noreply.github.com>
2022-12-20 12:29:52 -0700

    deleted unused code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,10 @@
         defer
       ></script>
 
+      <script src="{{ asset('../js/test.js') }}"></script>
+    
+      
+
       {{-- font awesome icons --}}
       {{-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"> --}}
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
@@ -141,64 +145,7 @@
 
         <div id="map"></div>
 
-
-        
-
-        <!-- The Modal -->
-        <div id="myModal" class="modal">
-
-          <!-- Modal content -->
-          {{-- <div class="modal-content">
-            <div class="text-center close" style="border:1px dashed #CCC; border-radius: 10px; padding: 10px">
-                <i class="fa fa-cloud-upload" aria-hidden="true"></i>
-                <p>Upload file here.</p>
-                <br>
-              <div class="text-center">
-                <input type="file" id="csvFile" accept=".csv"  />
-              </div>
-            </div>
-          </div> --}}
-
-        </div>
-
-        <script>
-          // Get the modal
-          var modal = document.getElementById("myModal");
-          
-          // Get the button that opens the modal
-          var btn = document.getElementById("modalButton");
-          
-          // Get the <span> element that closes the modal
-          var span = document.getElementsByClassName("close")[0];
-          
-          // When the user clicks the button, open the modal 
-          btn.onclick = function() {
-            modal.style.display = "block";
-          }
-          
-          // When the user clicks on <span> (x), close the modal
-          span.onclick = function() {
-            modal.style.display = "none";
-          }
-          
-          // When the user clicks anywhere outside of the modal, close it
-          window.onclick = function(event) {
-            if (event.target == modal) {
-              modal.style.display = "none";
-            }
-          }
-          </script>
-          <script>
-            function openNav() {
-              document.getElementById("mySidenav").style.width = "250px";
-            }
-            
-            function closeNav() {
-              document.getElementById("mySidenav").style.width = "0";
-            }
-            </script>
     </body>
-
     <script>
         // This example creates a 2-pixel-wide red polyline showing the path of
         // the first trans-Pacific flight between Oakland, CA, and Brisbane,
@@ -275,10 +222,8 @@
             map.setMapTypeId('satellite');
           }
         }
-
-        
-    
       </script>
+      
       <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
